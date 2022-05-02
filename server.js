84bd2557cf53e59973d95dc5969abe0dd3168c78
carlos valenzuela <carlos@aureolab.cl>
2022-05-02 14:05:43 -0400

    init

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const Multer = require('multer');
 
 var app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', Multer({
+  storage: Multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024, // no larger than 5mb, you can change as needed.
+  },
+}).single('upfile'), function (req, res) {
+  return res.send({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  })
+})
 
 
 const port = process.env.PORT || 3000;
